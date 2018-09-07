59793507a9cbd8fafd067a462df0767ebce179a0
sylvester_christian <c20christian.sylvester@usafa.edu>
2018-09-07 15:56:55 -0600

    Client working

--- Pex1/pex_server.py
+++ Pex1/pex_server.py
@@ -28,7 +28,7 @@ SONG2 = "Suzanne Vega - Toms Diner.mp3"
 
 
 # Accepts 4 bit integer value to the bitrate and 2 bit samplerate lookup
-bitrate_lookup = {0: -1, 1: 32000, 2: 40000, 3: 48000, 4: 56000, 5: 64000, 6: 80000, 7: 96000, 8: 112000, 9: 128000,
+bitrate_lookup = {0: 32000, 1: 32000, 2: 40000, 3: 48000, 4: 56000, 5: 64000, 6: 80000, 7: 96000, 8: 112000, 9: 128000,
                   10: 160000, 11: 192000, 12: 224000, 13: 256000, 14: 320000, 15: -1}
 samplerate_lookup = {0: 44100, 1: 48000, 2: 32000, 3: -1}
 
@@ -51,20 +51,27 @@ while True:
         message = bytes("LIST_REPLY\n" + SONG1 + "\n" + SONG2 + "\n\0", encoding='UTF-8')
         my_socket.sendto(message, client_address)
     # Streams the data
-    # TODO: find if client or server bug in stream (hangs until timeout)
     elif data[0:12].decode('UTF-8').upper() == 'START_STREAM':
         try:
-            if data[13:] != (SONG1 or SONG2):
+            if data[13:].decode('UTF-8') != (SONG1 or SONG2):
                 raise Mp3Error
             with open(data[13:].decode('UTF-8'), 'rb') as f:  # Grabs the file and puts it into a binary string
                 binary_song = f.read()
             file_start = binary_song.find(b'\xff\xfb', 0)
-            i = 0
+            i = file_start
             # Send Datagrams until the song is complete
             while i < len(binary_song):
-                message = bytes("STREAM_DATA\n") + binary_song[i:i + 32]
+                bitrate = bitrate_lookup[binary_song[i+4] >> 4]
+                samplerate = samplerate_lookup[binary_song[i+5] >> 6]
+                frame_size = int((144 * int(bitrate) / int(samplerate)) + int(binary_song[i+21] & 0x01))
+                message = bytes("STREAM_DATA\n", encoding='UTF-8') + binary_song[i:i + frame_size]
                 my_socket.sendto(message, client_address)
-                i += 32
+                if bitrate == -1 or samplerate == -1:
+                    i += 144 * int(320000 / 48000) + 1
+                i += frame_size
+                i = binary_song.find(b'\xff\xfb', i - 1)
+                if i < 0:
+                    break
             message = b'STREAM_DONE'
             my_socket.sendto(message, client_address)
         except Mp3Error:  # In the event a song name not in the options was entered
